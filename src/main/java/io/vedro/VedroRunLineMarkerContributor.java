d69d8a4bbb663ad5eca5f59a36917d5cb9b9b02c
Nikita Tsvetkov <tsv1@fastmail.com>
2025-05-03 17:12:11 +0400

    Refactor methods to use constants

--- src/main/java/io/vedro/VedroRunLineMarkerContributor.java
+++ src/main/java/io/vedro/VedroRunLineMarkerContributor.java
@@ -1,5 +1,10 @@
 package io.vedro;
 
+import javax.swing.Icon;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import com.intellij.execution.lineMarker.ExecutorAction;
 import com.intellij.execution.lineMarker.RunLineMarkerContributor;
 import com.intellij.icons.AllIcons;
@@ -7,26 +12,35 @@ import com.intellij.psi.PsiElement;
 import com.jetbrains.python.psi.PyClass;
 import com.jetbrains.python.psi.PyDecorator;
 import com.jetbrains.python.psi.PyFunction;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
 
 public class VedroRunLineMarkerContributor extends RunLineMarkerContributor{
     protected static Icon ICON_RUN_SCENARIO = AllIcons.RunConfigurations.TestState.Run;
     protected static Icon ICON_RUN_PARAMS = AllIcons.RunConfigurations.TestState.Run_run;
+    private static final String SCENARIO_BASE_CLASS = "vedro._scenario.Scenario";
+    private static final String PARAMS_DECORATOR_NAME = "params";
+    private static final String INIT_METHOD_NAME = "__init__";
+
+    @Override
+    public @Nullable Info getInfo(@NotNull PsiElement element) {
+        // Scenario class line marker
+        if (element instanceof PyClass && isScenario((PyClass) element)) {
+            return new Info(ICON_RUN_SCENARIO, ExecutorAction.getActions(), null);
+        }
+        // Params decorator line marker
+        if (element instanceof PyDecorator && isParams((PyDecorator) element)) {
+            return new Info(ICON_RUN_PARAMS, ExecutorAction.getActions(), null);
+        }
+        return null;
+    }
 
     protected boolean isScenario(PyClass cls) {
-        String clsName = String.valueOf(cls.getName());
-        if (!clsName.startsWith("Scenario") && !clsName.endsWith("Scenario")) {
-            return false;
-        }
-        return cls.isSubclass("vedro._scenario.Scenario", null);
+        return cls.isSubclass(SCENARIO_BASE_CLASS, null);
     }
 
     protected boolean isParams(PyDecorator decorator) {
         String decoratorName = String.valueOf(decorator.getName());
-        if (!decoratorName.equals("params")) {
+        if (!decoratorName.equals(PARAMS_DECORATOR_NAME)) {
             return false;
         }
 
@@ -34,23 +48,12 @@ public class VedroRunLineMarkerContributor extends RunLineMarkerContributor{
         if (target == null) {
             return false;
         }
-        String targetName = String.valueOf(target.getName());
-        if (!targetName.equals("__init__")) {
+        String targetName = target.getName();
+        if (!targetName.equals(INIT_METHOD_NAME)) {
             return false;
         }
 
-        PyClass cls = target.getContainingClass();
-        return cls != null && isScenario(cls);
-    }
-
-    @Override
-    public @Nullable Info getInfo(@NotNull PsiElement element) {
-        if (element instanceof PyClass && isScenario((PyClass) element)) {
-            return new Info(ICON_RUN_SCENARIO, ExecutorAction.getActions(), null);
-        }
-        if (element instanceof PyDecorator && isParams((PyDecorator) element)) {
-            return new Info(ICON_RUN_PARAMS, ExecutorAction.getActions(), null);
-        }
-        return null;
+        PyClass containingClass = target.getContainingClass();
+        return containingClass != null && isScenario(containingClass);
     }
 }
